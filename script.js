1574b6e458498bd177a8f89028bada7bfaaa79ba
dpk9911 <84388661+deepakchauhan26@users.noreply.github.com>
2021-10-22 22:27:35 +0530

    excel

---
+++ script.js
@@ -0,0 +1,29 @@
+let topRow =  document.querySelector(".top_row");
+for(let i = 0; i < 26; i++){
+    let div = document.createElement("div"
+    );
+    div.setAttribute("class", "cell");
+    div.textContent = String.fromCharCode(65 + i);
+    topRow.appendChild(div)
+}
+
+let leftCol = document.querySelector(".left_col");
+for(let i =1; i<= 100; i++){
+    let div = document.createElement("div");
+    div.setAttribute("class","cell");
+    div.textContent = i;
+    leftCol.appendChild(div)
+}
+let grid = document.querySelector(".grid");
+for(let i= 0; i < 100; i++){
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 0; j < 26; j++);{
+    let div = document.createElement("div");
+    div.setAttribute("class","cell"
+    );
+    div.textContent = i + "," +j;
+    row.appendChild(div);
+}
+grid.appendChild(row)
+}
